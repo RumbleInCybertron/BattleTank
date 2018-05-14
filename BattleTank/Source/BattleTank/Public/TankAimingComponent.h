2b892c3553d12cccc2a181195e0df10cfff0b397
Derek Haus <rumbleincybertron@gmail.com>
2018-05-14 20:28:54 +0900

    Decoupling the Architecture

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -28,7 +28,7 @@ public:
 	
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "State")
-	EFiringStatus FiringStatus = EFiringStatus::Aiming;
+	EFiringStatus FiringStatus = EFiringStatus::Locked;
 
 private:
 	// Sets default values for this component's properties
